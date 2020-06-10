49734805bd39fa5f83b1e2c2c207714e28be5e27
haritha-ambati <58062897+haritha-ambati@users.noreply.github.com>
2020-06-10 11:50:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/',(req,res)=>{
-  return res.send("Hi,This is Primary app !!!!!")
+  return res.send("Secondary app !!!!!")
 })
 
 app.listen(process.env.PORT||5000, () => console.log('Server started on port 5000'));
